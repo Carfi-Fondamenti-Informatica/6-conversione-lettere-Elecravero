8f87e08c2e154ec4c2b365f1f01f4665e8ca42ee
Elecravero <91529865+Elecravero@users.noreply.github.com>
2021-11-29 09:55:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "lib.h"
-
+using namespace std;
 int main(){
   char x;
     cin >> x;
     if (lettera (x) == true) {
-        cout << a << endl;
+        cout << x << endl;
     } else {
         cout << "errore" << endl;
         return 0;
