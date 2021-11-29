e0f71df764d853e0ad5d83444e275192b39dd322
Elecravero <91529865+Elecravero@users.noreply.github.com>
2021-11-29 09:37:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+bool lettera (char &a){
+    bool lett;
+    if (a>=65 and a<=90){
+        lett=true;
+        a=a+32;
+    }else if(a>=97 and a<=122){
+        lett=true;
+        a=a-32;
+    }else{
+        lett=false;
+    }
+    return lett;
+}
 
